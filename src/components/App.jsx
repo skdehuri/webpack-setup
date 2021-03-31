58f9cbde3b8314d2d1a66f647d58f75315f0349b
swaraj <swaraj.dehuri@gmail.com>
2021-03-31 22:14:48 +0530

    basic React App component and styling added

---
+++ src/components/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div>
+      <p>Hello from React</p>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-const message = "webpack setup added";
+import { render } from "react-dom";
+import App from "./components/App";
+import "./styles/index.scss";
 
+const message = "webpack setup added";
 console.log(message);
+
+render(<App />, document.getElementById("root"));
